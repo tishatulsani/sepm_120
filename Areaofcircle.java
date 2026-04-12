4fbeb8a3f0cab604ec47f887f3b714f985617928
tishatulsani <maggiee1406@gmail.com>
2026-04-12 18:07:40 +0530

    Add files via upload

---
+++ Areaofcircle.java
@@ -0,0 +1,17 @@
+//WAJP to calculate the area and circumference of a circle.Take hardcoded radius.
+class Areaofcircle{
+	public static void main(String[] args){
+		float radius=5,area,circumference;
+		float PI=3.14f;
+		area=PI*radius*radius;
+		circumference=2*PI*radius;
+		System.out.println("Area of circle:"+area);
+		System.out.println("Circumference of circle:"+circumference);
+		
+	}
+}
+
+/*MAM KA CODE 
+System.out.println("Area of circle:"+(PI*radius*radius));
+System.out.println("Circumference of circle:"+(2*PI*radius));
+		
